321877ea66d49a366d11fbd671fb8cb0d4856a18
fnya <fnyamail@gmail.com>
2022-08-27 20:04:21 +0900

    SlackApiClinet動作

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,23 @@
 import { container } from './inversify.config';
 import Types from '../../common-lib-for-slack/dist/lib/types/Types';
-import PropertyType from '../../common-lib-for-slack/dist/lib/types/PropertyType';
-import { ISlackApiClient } from '../../common-lib-for-slack/dist/lib/interface/ISlackApiClient';
+// import PropertyType from '../../common-lib-for-slack/dist/lib/types/PropertyType';
+import { SlackApiClient } from '../../common-lib-for-slack/dist/lib/util/SlackApiClient';
 
-export default function test() {
-  const slackApiClient = container.get<ISlackApiClient>(Types.ISlackApiClient);
-  const adminFolerId = slackApiClient.getProperty(PropertyType.AdminFolerId);
+export default async function test() {
+  const slackApiClient = container.get<SlackApiClient>(Types.SlackApiClient);
+  const messages = await slackApiClient.getMessages('C01152A36RZ');
+  for (const m of messages) {
+    const date = new Date(Number(m.ts) * 1000);
+    console.log(date.toLocaleString() + ':' + m.text);
+  }
 
-  console.log(adminFolerId);
+  const replies = await slackApiClient.getReplies(
+    'C01152A36RZ',
+    '1661481538.065389'
+  );
+
+  for (const m of replies) {
+    const date = new Date(Number(m.ts) * 1000);
+    console.log(date.toLocaleString() + ':' + m.text);
+  }
 }
