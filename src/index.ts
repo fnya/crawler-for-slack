8a913f26b03b60f3f844c8ef61b2afb56422b6bb
fnya <fnyamail@gmail.com>
2022-11-23 12:35:16 +0900

    npm公開用設定変更

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
 import 'reflect-metadata';
-/**
- * 1つのJavaScripにまとめたいTypeScriptを定義する
- */
-import { crawlChannels } from './crawlChannels';
-import { crawlMembers } from './crawlMembers';
-import { crawlMessages } from './crawlMessages';
-import { crawlReplies } from './crawlReplies';
-
-// globalの型定義
-declare const global: {
-  [x: string]: unknown;
-};
-
-// 公開したい関数を定義
-global.crawlChannels = crawlChannels;
-global.crawlMembers = crawlMembers;
-global.crawlMessages = crawlMessages;
-global.crawlReplies = crawlReplies;
+export * from '../src';
